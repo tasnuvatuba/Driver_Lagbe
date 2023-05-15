9ca02cafaf7b10c40d7e03f249bf04a85595f27c
tasnuvatuba <tasnuvatuba99@gmail.com>
2023-05-16 03:43:32 +0600

    sent and received request

--- pages/driverHomePage.js
+++ pages/driverHomePage.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+
 
 import { AboutUs, Review, Gallery, Footer, Header, Intro, Services } from './container';
 import { DriverNavbar } from './components';
 import './driverHomePage.css';
 
-const App = () => (
-  <div>
-    <DriverNavbar />
+const App = () => {
+  const router = useRouter();
+  const { username} = router.query;
+  console.log(username);
+
+  return (<div>
+    <DriverNavbar username = {username}/>
     <Header />
-    <Intro />
     <AboutUs />
     <Services />
     <Gallery/>
     <Footer />
   </div>
-);
+  );
+
+}
 
 export default App;
